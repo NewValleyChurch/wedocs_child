f826af14dd7da9eea72452b11502131394e6ffb6
arretx <jon@jongriffith.com>
2018-10-07 20:18:37 -0700

    Initial

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'wedocs_child_assets' );
+function wedocs_child_assets() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ));
+}
+function add_file_types_to_uploads($file_types){
+$new_filetypes = array();
+$new_filetypes['svg'] = 'image/svg+xml';
+$file_types = array_merge($file_types, $new_filetypes );
+return $file_types;
+}
+add_action('upload_mimes', 'add_file_types_to_uploads');
